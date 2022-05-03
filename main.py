d0c06e81cf7a9bcf4821cf9834c1c48da3018655
L1nkoln <95244116+L1nkoln69@users.noreply.github.com>
2022-05-03 22:58:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
-import json
-import os
-import re
+import asyncio
+
+import aiohttp
 import datetime
 import requests
 
-from bs4 import BeautifulSoup
-from termcolor import colored
+
+async def first(url, session):
+    response = await session.request(method='GET', url=url)
+    return await response.json()
 
 
-site_url = 'https://www.balldontlie.io/api/v1/games'
+async def main(url, session):
+    result = await first(url, session)
+    return int(result['team_all_season']['queryResults']['row'][0]['mlb_org_id'])
 
 
-def first():
-    res = (requests.get(site_url)).json()
-    res2 = (requests.get('https://weatherstack.com/')).text
+async def main2(url, session):
+    result = await first(url, session)
     list_ = []
     a = 0
-    for i in res['data']:
+    for i in result['data']:
         list_.append(i['home_team_score'])
         a += i['home_team_score']
-    len_i = int(len(list_))
-    char = a//len_i
-    print(res2)
+    len_i = len(list_)
+    char = a // len_i
     return char
+
+
+async def maxim():
+    async with aiohttp.ClientSession() as session:
+        result = await asyncio.gather(*[main("http://lookup-service-prod.mlb.com/json/named.team_all_season.bam?sport_code=%27mlb%27&all_star_sw=%27N%27&sort_order=name_asc&season=%272021%27", session),
+                                        main2('https://www.balldontlie.io/api/v1/games', session)])
+        res = sum(result)
+        len_res = len(result)
+    return res/len_res
 
 
 start = datetime.datetime.now()
 print('Start')
-# asyncio.run(main(SITE, TAG, datetime.datetime.now().strftime('%Y-%m-%d')))
-print(first())
+print(asyncio.run(maxim()))
 print("Done")
 print(datetime.datetime.now() - start)
